7c953459a6091f170b8e31ad2d7d0000642d4f2a
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2024-06-16 12:23:58 +0100

    this is the first push to the GDSC robot

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/brainboxtayo/new_practice_ws/src
+# Build directory: /home/brainboxtayo/new_practice_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_description")
